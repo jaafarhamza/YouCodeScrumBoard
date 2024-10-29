acfa5b5fc089d4b2e6fff78e87c1a2ccc4965235
jaafarhamza <jaafar.hamza711@gmail.com>
2024-10-29 17:38:29 +0100

    push

---
+++ starter/assets/js/script.js
@@ -0,0 +1,13 @@
+console.log('hello');
+console.log('hello');
+
+let address, zip, state;
+address = 'casablanca';
+zip = '4000';
+state = 'moroco';
+
+const fullName = 'hamza';
+const lastName = 'jaafar';
+let firstName = fullName + ' ' + lastName;
+
+console.log('your name is :' + firstName);
